375a5f7648180f7ea7db1b2f7174adb171d47d92
n25004 <n25004@std.it-college.ac.jp>
2025-12-18 12:00:46 +0900

    compornent

---
+++ app/libs/microms.ts
@@ -0,0 +1,11 @@
+export type category = {
+  name: string;
+};
+
+export type News = {
+  id: string;
+  title: string;
+  category: { name: string };
+  publishedAt: string;
+  createdAt: string;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,95 @@
-export default function Home(){
-  return <h1>テクノロジーの力で世界を変える</h1>;
+import Image from "next/image";
+import styles from "./page.module.css";
+import BottonLink from "@/app/compornents/BottonLink";
+import NewsList from "./compornents/NewsList";
+import { News } from "@/app/libs/microms";
+
+const data: { contents: News[] } = {
+  contents: [
+    {
+      id: "1",
+      title: "渋谷にオフィスを移転しました",
+      category: { name: "更新情報" },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "2",
+      title: "当社CEOが業界トップリーダーTOP30に選出されました",
+      category: { name: "更新情報" },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "3",
+      title: "テストの記事です",
+      category: { name: "更新情報" },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+  ],
+};
+
+export default function Home() {
+  const slicedData = data.contents.slice(0, 2);
+  const name = "世界";
+
+  return (
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+          <p className={styles.description}>
+            私達は市場をリードしているグリーバルデックカンパニーです
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+          priority
+        />
+      </section>
+
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <ul>
+          {slicedData.map((article) => (
+            <li key={article.id} className={styles.newsItem}>
+              <div className={styles.link}>
+                <Image
+                  className={styles.image}
+                  src="/no-image.png"
+                  alt="No image"
+                  width={1200}
+                  height={630}
+                />
+                <dl className={styles.contents}>
+                  <dt className={styles.newsItemTitle}>{article.title}</dt>
+                  <dd className={styles.meta}>
+                    <span className={styles.tag}>{article.category.name}</span>
+                    <span className={styles.date}>
+                      <Image
+                        src="/clock.svg"
+                        alt=""
+                        width={16}
+                        height={16}
+                        priority
+                      />
+                      {article.publishedAt}
+                    </span>
+                  </dd>
+                </dl>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className={styles.newsLink}>
+          <BottonLink href="/news">もっと見る</BottonLink>
+        </div>
+      </section>
+    </>
+  );
 }
